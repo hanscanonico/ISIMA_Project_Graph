1169d4bd1e95f44a46ba1e31d6e0ec19a221002e
RomanceDawn <enix70@msn.com>
2014-03-10 20:43:55 +0100

    Fonction tracertest

--- ApplicationGraphes/src/presentation/MonPanel.java
+++ ApplicationGraphes/src/presentation/MonPanel.java
@@ -10,9 +10,18 @@ public class MonPanel extends JPanel{
             
                 g.setColor(Color.WHITE);
 		g.fillRect(0, 0, this.getWidth(), this.getHeight());
-                g.setColor(Color.RED);
-                g.drawOval(50, 50, 50, 50);
+                
+               // g.drawOval(50, 50, 50, 50);
+                this.tracerCercle(g);
+                g.setColor(Color.blue);
                 g.drawOval(150, 50, 50, 50);
                 g.drawLine(100, 75, 150,75);
+         
 	}
+        
+        private void tracerCercle(Graphics g){
+            g.setColor(Color.red);
+            g.drawOval(50, 50, 50, 50);
+        }
+        
 }
