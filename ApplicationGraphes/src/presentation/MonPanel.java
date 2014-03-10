4f6782eb3924f0f59a1bbb5513f45805fc9744f4
RomanceDawn <enix70@msn.com>
2014-03-10 21:25:39 +0100

    antialiasing
    antialiasing

--- ApplicationGraphes/src/presentation/MonPanel.java
+++ ApplicationGraphes/src/presentation/MonPanel.java
@@ -2,24 +2,30 @@ package presentation;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D; 
+import java.awt.RenderingHints;
 
 import javax.swing.JPanel;
 
 public class MonPanel extends JPanel{
 	public void paintComponent(Graphics g){
             
-                g.setColor(Color.WHITE);
-		g.fillRect(0, 0, this.getWidth(), this.getHeight());
+            Graphics2D g2 = (Graphics2D)g;
+   g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                        RenderingHints.VALUE_ANTIALIAS_ON);
+                        
+                g2.setColor(Color.WHITE);
+		g2.fillRect(0, 0, this.getWidth(), this.getHeight());
                 
                // g.drawOval(50, 50, 50, 50);
-                this.tracerCercle(g);
-                g.setColor(Color.blue);
-                g.drawOval(150, 50, 50, 50);
-                g.drawLine(100, 75, 150,75);
+                this.tracerCercle(g2);
+                g2.setColor(Color.blue);
+                g2.drawOval(150, 50, 50, 50);
+                g2.drawLine(100, 75, 150,75);
          
 	}
         
-        private void tracerCercle(Graphics g){
+        private void tracerCercle(Graphics2D g){
             g.setColor(Color.red);
             g.drawOval(50, 50, 50, 50);
         }
