dc2d719de746605c125c2e369cb85768e4577290
RomanceDawn <enix70@msn.com>
2014-03-13 18:26:03 +0100

    modfi du   test

--- ApplicationGraphes/src/metier/Test.java
+++ ApplicationGraphes/src/metier/Test.java
@@ -30,16 +30,33 @@ public class Test {
         Sommet b = new Sommet("B", 2);
         Sommet c = new Sommet("C", 3);
         Sommet d = new Sommet("D", 3);
+        Sommet e = new Sommet("E", 3);
         Arrete ab = new Arrete(a, b);
         Arrete ac = new Arrete(a, c);
         Arrete bc = new Arrete(b, c);
+        Arrete ad = new Arrete(a, d);
+        Arrete bd = new Arrete(b, d);
+        Arrete cd = new Arrete(c, d);
+        Arrete ae = new Arrete(a, e);
+        Arrete be = new Arrete(b, e);
+        Arrete ce = new Arrete(c, e);
+        Arrete de = new Arrete(d, e);
+
         g.ajouterSommet(a);
         g.ajouterSommet(b);
         g.ajouterSommet(c);
         g.ajouterSommet(d);
+        g.ajouterSommet(e);
         g.ajouterArrete(ab);
         g.ajouterArrete(ac);
         g.ajouterArrete(bc);
+        g.ajouterArrete(ad);
+        g.ajouterArrete(bd);
+        g.ajouterArrete(cd);
+        g.ajouterArrete(ae);
+        g.ajouterArrete(be);
+        g.ajouterArrete(ce);
+        g.ajouterArrete(de);
         Fenetre fen = new Fenetre(g);
 
 
